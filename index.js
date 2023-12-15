51536c460664b21404fadb43ae49cab6bc4fee34
Sleezzi <ilan.dfp91@gmail.com>
2023-12-15 15:06:14 +0100

    .

---
+++ index.js
@@ -0,0 +1,55 @@
+const bannedWord = [
+    "<",
+    ">",
+];
+
+const badges = {
+    broadcaster: "https://static-cdn.jtvnw.net/badges/v1/5527c58c-fb7d-422d-b71b-f309dcb85cc1/1",
+    moderator: "https://static-cdn.jtvnw.net/badges/v1/3267646d-33f0-4b17-b3df-f923a41db1d0/1",
+    subscriber: "https://cdn-icons-png.flaticon.com/512/929/929424.png",
+    premium: "https://static-cdn.jtvnw.net/badges/v1/bbbe0db0-a598-423e-86d0-f9fb98ca1933/1",
+    "no_audio": "https://static-cdn.jtvnw.net/badges/v1/aef2cd08-f29b-45a1-8c12-d44d7fd5e6f0/1",
+    "no_video": "https://static-cdn.jtvnw.net/badges/v1/199a0dba-58f3-494e-a7fc-1fa0a1001fb8/1",
+    partener: "https://static-cdn.jtvnw.net/badges/v1/d12a2e27-16f6-41d0-ab77-b780518f00a3/1",
+    "glhf-pledge": "https://static-cdn.jtvnw.net/badges/v1/3158e758-3cb4-43c5-94b3-7639810451c5/1",
+    "twitch-recap-2023": "https://static-cdn.jtvnw.net/badges/v1/4d9e9812-ba9b-48a6-8690-13f3f338ee65/1",
+}
+
+// const tmi = require("tmi.js");
+const client = new tmi.Client({
+	options: { debug: true },
+    connection: {
+        secure: true,
+        reconnect: true
+    },
+	channels: [ "Sleezzi" ]
+});
+const setDate = () => {
+    document.querySelector("#compteur").innerHTML = `${(new Date().getHours() > 9 ? new Date().getHours() : `0${new Date().getHours()}`)}:${(new Date().getMinutes() > 9 ? new Date().getMinutes() : `0${new Date().getMinutes()}`)}`;
+}
+
+client.on("clearchat", () => {
+    document.querySelector("#chat > #messageContainer").innerHTML = "";
+});
+
+client.on("message", (channel, tags, message, self) => {
+    console.log(Object.keys(tags.badges));
+    if ((message === "!clear" || message === "!cls") && (tags.mod || (tags.badges && tags.badges.brodcaster !== null))) return document.querySelector("#chat > #messageContainer").innerHTML = "";
+    for (word of bannedWord) if (message.toLowerCase().includes(word)) message = message.toLowerCase().replace(word, "#".repeat(word.length));
+    const messageContainer = document.createElement("div");
+    messageContainer.className = "message";
+    messageContainer.innerHTML = `
+<div class="author">
+    ${Object.keys(tags.badges).map(badge => (badge && badges[badge] ? `<img src="${badges[badge]}"></img>` : ""))}
+    <${(tags.mod || tags.subscriber ? "strong" : "span")} style="color: ${tags.color};">${tags.username}:</${(tags.mod || tags.subscriber ? "strong" : "span")}>
+</div>
+<div class="content">
+    <${(tags.mod || tags.subscriber ? "strong" : "span")}>${message}</${(tags.mod || tags.subscriber ? "strong" : "span")}>
+</div>`;
+    document.querySelector("#chat > #messageContainer").appendChild(messageContainer);
+});
+
+setDate();
+setInterval(setDate, 30_000);
+
+client.connect().catch(err => console.error(err));
